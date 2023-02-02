ce96e44b497e21ccd750477c739c083036547645
jaozinmelo <joaopedrodemartins440@gmail.com>
2023-02-01 22:49:55 -0300

    exercicio operadores

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,57 @@
-//CODIGO AQUI
+//PRATICA GUIADA 1
+/* let saldo
+console.log(`Valor do saldo inicial ${saldo}`)
+saldo = 15
+
+saldo = saldo + 22.40
+console.log(`Valor do saldo após a primeira operação: ${saldo}`)
+
+saldo = saldo - 12.34
+console.log(`Valor do saldo após a segunda  operação: ${saldo}`)
+
+saldo = saldo / 3
+console.log(`Valor do saldo após a terceira operação: ${saldo}`)
+ 
+saldo = saldo * 3.42
+console.log(`Valor do saldo após a quarta operação: ${saldo}`)
+
+saldo = saldo % 3
+console.log(`Valor do saldo após a quinta  operação: ${saldo}`) */
+
+//PRATICA GUIADA 2
+/* const resultado1 = 5 === 5
+console.log(resultado1)
+
+const resultado2 = 5 !== 5
+console.log(`Comparação 2:
+${resultado2}`)
+
+const resultado3 = 5 !== "5"
+console.log(`Comparação 3:
+${resultado3}`)
+
+const resultado4 = '5' === 'cinco'
+console.log(`Comparação 4:
+${resultado4}`)
+
+const resultado5 = typeof 5 === typeof 20
+console.log(`Comparação 5:
+${resultado5}`)
+
+const resultado6 =typeof '5' === typeof'cinco'
+console.log(`Comparação 6:
+${resultado6}`)
+ */
+
+//PRATICA GUIADA 3
+// > < >= <=
+/* 
+const primeiroNumero = 13
+const segundoNumero = 22
+console.log(`Primeiro Numero : ${primeiroNumero}
+Segundo Numero : ${segundoNumero}`)
+
+let comparação = primeiroNumero > segundoNumero
+console.log(`O primeiro numero é maior que o segundo numero ? 
+${comparação}`) */
+
